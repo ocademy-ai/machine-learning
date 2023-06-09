4d0a10ced77fdc29f02a678f26124d97751538cb
Zikang CHEN <appointat@shu.edu.cn>
2023-06-09 22:45:22 +0200

    Update the source link updator

--- chatbot/workflows/update_source_link.py
+++ chatbot/workflows/update_source_link.py
@@ -1,8 +1,18 @@
 import os
-from ..prep_data import prep_data
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from prep_data import prep_data
+
+
 
 def main():
     # Update the source links.
+    update_source_link()
+    # Update the embeddings of text.
+    prep_data()
+
+
+def update_source_link():
     path = 'open-machine-learning-jupyter-book'
     folders = []
     md_files = []
@@ -15,7 +25,7 @@ def main():
                 folders.append(dir)
 
     for folder in folders:
-        file_content = ""
+        file_content = f"#### {folder}:\n"
         folder_md_files = []
 
         for md_file in md_files:
@@ -24,13 +34,12 @@ def main():
                 folder_md_files.append(f"'https://github.com/open-academy/machine-learning/tree/main/{md_file}',\n")
 
         file_content += ''.join(folder_md_files)
+        #file_content += '\n'
 
         if folder_md_files:
             with open(r'chatbot\vector-db-persist-directory\resources\{}.txt'.format(folder), 'w') as f:
                 f.write(file_content)
 
-    # Update the embeddings of text
-    prep_data()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
