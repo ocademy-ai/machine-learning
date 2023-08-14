33d23221f16aa138db94a97bb1ce981e7443f9d0
黄永辉 <huangyh__123@163.com>
2023-08-14 09:20:16 +0800

    add foreign keys

--- awesome/database/constants.js
+++ awesome/database/constants.js
@@ -11,6 +11,17 @@ const TABLES = {
   USERCOURSES: 'UserCourses',
 }
 
+async function alterTable(knex, tableName, tableBuilderFunction) {
+  try {
+      await knex.schema.alterTable(tableName, tableBuilderFunction);
+      console.log(`Table "${tableName}" altered successfully.`);
+  } catch (error) {
+      console.error("Error altering table:", error);
+  } finally {
+  }
+}
+
 module.exports = {
   TABLES,
+  alterTable
 };
