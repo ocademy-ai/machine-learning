2169ee15de940de11f73f8cdfbe933850066edbe
LongfeiH <2069860997@qq.com>
2023-08-20 18:02:12 +0800

    Add association between id fields

---
+++ awesome/database/migrations/20230820065806_020_insert_directus_fields_UserCourses_courseId.js
@@ -0,0 +1,36 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex('directus_fields').insert({
+      collection: 'UserCourses',
+      field: 'courseId',
+      special: null,
+      interface: 'select-dropdown-m2o',
+      options: '{"template":"{{id}}"}',
+      display: null,
+      display_options: null,
+      readonly: 0,
+      hidden: 0,
+      sort: null,
+      width: 'full',
+      translations: null,
+      note: null,
+      conditions: null,
+      required: 0,
+      group: null,
+      validation: null,
+      validation_message: null
+    });
+  };
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex('directus_fields')
+      .where('id', 141)
+      .del();
+  };
