4d47f634623e3e2f185a084ff1c3579dfce90468
296406598 <296406598@qq.com>
2023-09-11 16:05:10 +0800

    add data and modify linear-and-polynomial-regression.ipynb

---
+++ open-machine-learning-jupyter-book/assets/html/polynomial-regression-demo/basic.js
@@ -0,0 +1,126 @@
+let screen_width = window.innerWidth, screen_height = window.innerHeight;
+let canvas_width, canvas_height;
+let fps = 24, paused = false;
+let mobile;
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    mobile = true;
+} else {
+    mobile = false;
+}
+
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
+
+let m_input = getElement("m-input");
+let m_display = getElement("m-display");
+let c_display = getElement("c-display");
+
+let warn_display = getElement("warn-display");
+if (mobile) {
+    canvas_width = 0.8 * screen_width;
+}
+else {
+    canvas_width = 0.4 * screen_width;
+}
+canvas_height = canvas_width;
+
+canvas.width = canvas_width;
+canvas.height = canvas_height;
+
+let animate = window.requestAnimationFrame
+    || window.webkitRequestAnimationFrame
+    || window.mozRequestAnimationFrame
+    || function (callback) {
+        window.setTimeout(callback, 1000 / fps);
+    };
+
+function step() {
+    if (!updated) {
+        update();
+        render();
+    }
+    animate(step);
+}
+
+window.onload = function () {
+    initParams();
+    animate(step);
+}
+
+let click_x, click_y, pressed;
+
+if (mobile) {
+    canvas.addEventListener("touchstart", function (e) {
+        getTouchPosition(canvas, e);
+        let touch = e.touches[0];
+        let mouseEvent = new MouseEvent("mousedown", {
+            clientX: touch.clientX,
+            clientY: touch.clientY
+        });
+        canvas.dispatchEvent(mouseEvent);
+        pressed = true;
+        clicked();
+    }, false);
+
+    canvas.addEventListener("touchmove", function (e) {
+        getTouchPosition(canvas, e);
+        let touch = e.touches[0];
+        let mouseEvent = new MouseEvent("mousemove", {
+            clientX: touch.clientX,
+            clientY: touch.clientY
+        });
+        canvas.dispatchEvent(mouseEvent);
+        moved();
+    }, false);
+
+    canvas.addEventListener("touchend", function (e) {
+        getTouchPosition(canvas, e);
+        let touch = e.touches[0];
+        let mouseEvent = new MouseEvent("mouseup", {
+            clientX: touch.clientX,
+            clientY: touch.clientY
+        });
+        canvas.dispatchEvent(mouseEvent);
+        pressed = false;
+        released();
+    }, false);
+}
+else {
+    canvas.addEventListener("mousedown", function (e) {
+        getMousePosition(canvas, e);
+        pressed = true;
+        clicked();
+    });
+
+    canvas.addEventListener("mousemove", function (e) {
+        getMousePosition(canvas, e);
+        moved();
+    });
+
+    canvas.addEventListener("mouseup", function (e) {
+        getMousePosition(canvas, e);
+        pressed = false;
+        released();
+    });
+
+    window.addEventListener("keydown", function (e) {
+        keyPressed(e.keyCode);
+    }, false);
+
+    window.addEventListener("keydown", function (e) {
+        keyReleased(e.keyCode);
+    }, false);
+}
+
+function getMousePosition(canvas, event) {
+    rect = canvas.getBoundingClientRect();
+    click_x = event.clientX - rect.left;
+    click_y = event.clientY - rect.top;
+}
+
+function getTouchPosition(canvas, event) {
+    var rect = canvas.getBoundingClientRect();
+    click_x = event.touches[0].clientX - rect.left;
+    click_y = event.touches[0].clientY - rect.top;
+}
